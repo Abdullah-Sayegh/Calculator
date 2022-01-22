f8590b2c7d711d360c9326722cf1447f46b5c227
Abdullah Sayegh <91585279+Abdullah-Sayegh@users.noreply.github.com>
2022-01-23 01:11:20 +0300

    Update calculator_en.py
    Hello,
    I have been added the (power of numbers) and (Make the code BETTER) than the first one
    Regards and Thanks

--- calculator_en.py
+++ calculator_en.py
@@ -1,19 +1,25 @@
 #This calculator is in english strings
 num1 = float(input("Calculater: Enter the first numer. You: "))
-symbol = input("Calculater: Enter symbol. You: ")
-num2 = float(input("Calculater: Enter the second numer. You: "))
+while True:
+    symbol = input("Calculater: Enter symbol. You: ")
+    if symbol == "=":
+        print(num1)
+        break
+    num2 = float(input("Calculater: Enter the second numer. You: "))
 
-if symbol == "+":
-    print(num1 + num2)
+    if symbol == "+":
+        num1 = num1 + num2
 
-elif symbol == "-":
-    print(num1 - num2)
+    elif symbol == "-":
+        num1 = num1 - num2
 
-elif symbol == "*":
-    print(num1 * num2)
+    elif symbol == "*":
+        num1 = num1 * num2
 
-elif symbol == "/":
-    print(num1 / num2)
+    elif symbol == "/":
+        num1 = num1 / num2
 
+    elif symbol == "^":
+        num1 = num1 ** num2
 
-else:print("Wrong operator try agin")
+    else:print("Wrong operator try agin")
